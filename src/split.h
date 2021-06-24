94c45ec076d50192e7168fd85016f32172c370bc
Fgarcia19 <40769836+Fgarcia19@users.noreply.github.com>
2021-06-24 16:43:49 -0500

    Add files via upload

---
+++ src/split.h
@@ -0,0 +1,52 @@
+#include "mlp.h"
+#include<stdlib.h>
+#include<time.h>
+
+struct SPLIT
+{
+    VECTOR y_train,y_test;
+    MATRIX x_train, x_test;
+
+    SPLIT(VECTOR labels, MATRIX dataset, float k)
+    {
+
+
+        int maximo = labels.size();
+        int train =labels.size()*k;
+        int test = labels.size()-train;
+
+        //creamos arreglo
+        vector<int> v;
+
+        while(v.size() != train)
+        {
+            int numero = rand()%labels.size();
+            if(find(v.begin(),v.end(),numero)==v.end())
+            {
+                v.push_back(numero);
+                x_train.push_back(dataset[numero]);
+                y_train.push_back(labels[numero]);
+            }
+        }
+
+        sort(v.begin(),v.end());
+        auto aux = v.begin();
+        int i = 0;
+        while(i!=labels.size())
+        {   
+            if(i==*aux)
+            {
+                aux++;
+            }
+            else
+            {
+                x_test.push_back(dataset[i]);
+                y_test.push_back(labels[i]);
+
+            }
+            i++;
+        }
+    }
+
+
+};
